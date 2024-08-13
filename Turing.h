7d0bcdaa26fd96356050626700402eb59cd2f613
PeterMai <89789068+IsPeterHere@users.noreply.github.com>
2024-08-13 21:40:49 +0100

    Add files via upload

--- Turing.h
+++ Turing.h
@@ -12,11 +12,11 @@ namespace Turing
         right,
     };
 
-    template <typename sate_T, typename alphabet_T>
+    template <typename sate_set_T, typename symbol_set_T>
     struct State_Symbol
     {
-        sate_T state;
-        alphabet_T symbol;
+        sate_set_T state;
+        symbol_set_T symbol;
 
         std::string toString() const {
             return std::to_string(state) + std::to_string(symbol);
@@ -26,24 +26,24 @@ namespace Turing
             return std::hash<std::string>()(toString());
         }
 
-        bool operator==(const State_Symbol< sate_T, alphabet_T>& other) const {
+        bool operator==(const State_Symbol< sate_set_T, symbol_set_T>& other) const {
             return state == other.state && symbol == other.symbol;
         }
     };
 
-    template <typename sate_T, typename alphabet_T>
+    template <typename sate_set_T, typename symbol_set_T>
     struct State_Symbol_Movement
     {
-        sate_T state;
-        alphabet_T symbol;
+        sate_set_T state;
+        symbol_set_T symbol;
         Movement move;
     };
 
-    template <typename sate_T, typename alphabet_T>
+    template <typename sate_set_T, typename symbol_set_T>
     struct Rules
     {
-        using SS = State_Symbol<sate_T, alphabet_T>;
-        using SSM = State_Symbol_Movement<sate_T, alphabet_T>;
+        using SS = State_Symbol<sate_set_T, symbol_set_T>;
+        using SSM = State_Symbol_Movement<sate_set_T, symbol_set_T>;
 
         std::unordered_map<SS, SSM> map{};
 
@@ -52,7 +52,7 @@ namespace Turing
             map[State_symbolKey] = State_symbol_MoveValue;
         }
 
-        void add(sate_T StateKey, alphabet_T symbolKey, sate_T StateValue, alphabet_T symbolValue, Movement MovementValue)
+        void add(sate_set_T StateKey, symbol_set_T symbolKey, sate_set_T StateValue, symbol_set_T symbolValue, Movement MovementValue)
         {
             map[{StateKey, symbolKey}] = { StateValue,symbolValue,MovementValue };
         }
@@ -60,58 +60,60 @@ namespace Turing
         SSM& operator[](SS& key) { return map[key]; }
     };
 
-    template <typename sate_T, typename alphabet_T, int tape_size_T>
+    template <typename sate_set_T, typename symbol_set_T, int tape_size_T>
     class Turing_Machine
     {
     public:
 
-        using RuleSet = Rules<sate_T, alphabet_T>;
-        using SS = State_Symbol<sate_T, alphabet_T>;
-        using SSM = State_Symbol_Movement<sate_T, alphabet_T>;
+        using RuleSet = Rules<sate_set_T, symbol_set_T>;
+        using SS = State_Symbol<sate_set_T, symbol_set_T>;
+        using SSM = State_Symbol_Movement<sate_set_T, symbol_set_T>;
 
-        Turing_Machine(std::vector<alphabet_T>& input, sate_T finish_state, RuleSet& rules) 
-            : input_sequence{ input },
+        Turing_Machine(sate_set_T finish_state, RuleSet& rules) 
+            :
               final_state{ finish_state },
               transformation_rules{ rules }
         {
-            assert((std::size(input) < tape_size) && "Input sequence is longer than given tape length");
         }
 
         //The return will be a string containg charcters taken from the leftmost cell to the position if the machines head.
-        std::vector<alphabet_T> boot_up()
+        std::vector<symbol_set_T> boot_up(std::vector<symbol_set_T>& input)
         {
+            assert((std::size(input) < tape_size) && "Input sequence is longer than given tape length");
+            input_sequence = &input;
+
             reset();
 
             //The input is inserted into the tape from the leftmost cell.
-            for (int i{}; i < std::ssize(input_sequence); i++)
+            for (int i{}; i < std::ssize(*input_sequence); i++)
             {
-                tape[i] = input_sequence[i];
+                tape[i] = (*input_sequence)[i];
             }
             while (state != final_state)
             {
                 step();
             }
 
-            return std::vector<alphabet_T>(tape.begin(), tape.end() - (tape_size - position));
+            return std::vector<symbol_set_T>(tape.begin(), tape.end() - (tape_size - position));
         }
 
     private:
         //Pre-Run-Time Setup
-        std::vector<alphabet_T> input_sequence;
+        std::vector<symbol_set_T>* input_sequence {};
         const std::size_t tape_size{ static_cast<std::size_t>(tape_size_T) };
-        sate_T final_state;
+        sate_set_T final_state;
         RuleSet transformation_rules;
 
         //Run-Time Enviroment
         int position{ 0 };
-        std::vector<alphabet_T> tape{ std::vector<alphabet_T>(tape_size) };
-        sate_T state{};
+        std::vector<symbol_set_T> tape{ std::vector<symbol_set_T>(tape_size) };
+        sate_set_T state{};
 
         void reset()
         {
             position = 0;
-            std::fill(tape.begin(), tape.end(), static_cast<alphabet_T>(0));
-            state = static_cast<sate_T>(0);
+            std::fill(tape.begin(), tape.end(), static_cast<symbol_set_T>(0));
+            state = static_cast<sate_set_T>(0);
         }
 
         void step()
@@ -143,10 +145,10 @@ namespace Turing
 }
 
 
-template<typename sate_T, typename alphabet_T>
-struct std::hash<Turing::State_Symbol< sate_T,  alphabet_T>>
+template<typename sate_set_T, typename symbol_set_T>
+struct std::hash<Turing::State_Symbol< sate_set_T,  symbol_set_T>>
 {
-    std::size_t operator()(const Turing::State_Symbol< sate_T,  alphabet_T>& s) const noexcept
+    std::size_t operator()(const Turing::State_Symbol< sate_set_T,  symbol_set_T>& s) const noexcept
     {
         std::size_t h = s.hash();
         return h;
